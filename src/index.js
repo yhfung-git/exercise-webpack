0aca7b673c4c1e6362a5fb58b7cde5a774eb5228
yonghowfung <yhfung940217@gmail.com>
2023-06-12 13:09:17 +0200

    added photo and fonts

--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
 import _ from "lodash";
 import "./style.css";
+import Forest from "./forest.png";
 
 const component = () => {
   const element = document.createElement("div");
 
   element.innerHTML = _.join(["Hello", "webpack"], " ");
   element.classList.add("hello");
+
+  const myPhoto = new Image();
+  myPhoto.src = Forest;
+  element.appendChild(myPhoto);
 
   return element;
 };
--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,25 @@
-const path = require('path');
+const path = require("path");
 
 module.exports = {
-  entry: './src/index.js',
+  entry: "./src/index.js",
   output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
   },
-  mode: 'development',
+  mode: "development",
   module: {
     rules: [
       {
         test: /\.css$/i,
-        use: ['style-loader', 'css-loader'],
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: "asset/resource",
       },
     ],
   },
