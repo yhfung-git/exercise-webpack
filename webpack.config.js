f1c77a1f594ecf6b70e81fd21d1208342c4fc719
yonghowfung <yhfung940217@gmail.com>
2023-06-12 11:39:20 +0200

    installed css and style loader

--- webpack.config.js
+++ webpack.config.js
@@ -3,8 +3,17 @@ const path = require('path');
 module.exports = {
   entry: './src/index.js',
   output: {
-    filename: 'main.js',
+    // filename: 'main.js',
+    filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
   },
   mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
 };
