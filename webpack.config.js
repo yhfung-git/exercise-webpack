c197226bbf09612ab0941ac8fe9492cf261fba27
yonghowfung <yhfung940217@gmail.com>
2023-06-12 14:16:03 +0200

    HtmlWebpackPlugin set up

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,18 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   entry: {
-    index: './src/index.js',
-    print: './src/print.js',
+    index: "./src/index.js",
+    print: "./src/print.js",
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "Output Management",
+    }),
+  ],
   output: {
-    filename: '[name].bundle.js',
+    filename: "[name].bundle.js",
     path: path.resolve(__dirname, "dist"),
   },
   mode: "development",
