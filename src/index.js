ca7d89022a0326838bb2ca98762de937c5c7ddf7
yonghowfung <yhfung940217@gmail.com>
2023-06-12 11:56:41 +0200

    bundle css

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import _ from "lodash";
+import "./style.css";
 
 const component = () => {
-  const element = document.createElement('div');
+  const element = document.createElement("div");
 
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  element.innerHTML = _.join(["Hello", "webpack"], " ");
+  element.classList.add("hello");
 
   return element;
-}
+};
 
 document.body.appendChild(component());
